30eac14fdba26c8c18e4ebe9bcf6820a44fa552f
Zach Ploskey <zach@ploskey.com>
2018-05-23 12:26:58 -0700

    Sort Neatline exhibits by title on browse page
    Uses the browse_params filter because the default_sort filter is
    broken for Neatline because of where they set the default sort
    field and direction.

--- MappingNorthlakePlugin.php
+++ MappingNorthlakePlugin.php
@@ -12,9 +12,10 @@ class MappingNorthlakePlugin extends Omeka_Plugin_AbstractPlugin
 {
 
     protected $_filters = array(
-        'items_browse_default_sort',
         'collections_browse_default_sort',
         'exhibits_browse_default_sort',
+        'items_browse_default_sort',
+        'neatline_exhibits_browse_params',
     );
 
     protected $_recordTitleSort = array('Dublin Core,Title', 'ASC');
@@ -50,6 +51,24 @@ class MappingNorthlakePlugin extends Omeka_Plugin_AbstractPlugin
     public function filterExhibitsBrowseDefaultSort($sortArray, $args)
     {
         return array('title', 'ASC');
+    }
+
+    /**
+     * Sort Neatline Exhibit browsing results by title.
+     *
+     * Note: using neatline_exhibits_browse_default_sort will not work because
+     * Neatline sets the sort direction prior to that filter, and the filter
+     * does not run if a sort field is set. This is a neatline bug that should
+     * probably be fixed. TODO: File a Neatline bug for this.
+     *
+     * @param Array $params
+     * @return Array
+     */
+    public function filterNeatlineExhibitsBrowseParams($params)
+    {
+        $params['sort_field'] = 'title';
+        $params['sort_dir'] = 'a';
+        return $params;
     }
 
 }
